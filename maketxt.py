97d7200f9c32aead1230e8b1f55ae7f8c1e7a56f
lucafeneziani <luca.feneziani@detector-med.com>
2023-10-18 10:24:30 +0200

    unnecessary files deleted

--- maketxt.py
+++
@@ -1,6 +0,0 @@
-f = open('configuration_params.txt','w')
-
-f.write('detector_type\tQUBENext\nintegral_anode\tyes\nstrip_anode\tyes\nn_channels\t128\nn_strips\t127\nrange_pitch\t5\nstrip_pitch\t1\nstrip_integral\tyes\n')
-
-f.close()
-
